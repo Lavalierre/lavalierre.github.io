e4cf5f1278e5cccaef9b54638d5bb27b8f134a90
RTSR <delavalierre@gmail.com>
2022-08-21 07:19:29 +0300

    test

--- init.js
+++ init.js
@@ -3,20 +3,20 @@ const { execSync } = require("child_process");
 const main = () => {
     try {
         // clone fork
-        execSync('git clone https://github.com/trade-helper/awakened-poe-trade-web-version.git')
+        execSync('git clone https://github.com/Lavalierre/awakened-poe-trade')
 
         // build app and move it to page root
-        execSync('cd awakened-poe-trade-web-version/renderer && npm install && npm run make-index-files && npm run build');
-        execSync('cp -r awakened-poe-trade-web-version/renderer/dist/. .');
+        execSync('cd awakened-poe-trade/renderer && npm install && npm run make-index-files && npm run build');
+        execSync('cp -r awakened-poe-trade/renderer/dist/. .');
     } finally {
         // cleanup
-        execSync('rm -rf awakened-poe-trade-web-version')
+        execSync('rm -rf awakened-poe-trade')
     }
 };
 
 process.on('SIGTERM', () => {
     // cleanup in case of app termination
-    execSync('rm -rf awakened-poe-trade-web-version')
+    execSync('rm -rf awakened-poe-trade')
 });
 
 main();
